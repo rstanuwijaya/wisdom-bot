4024ab451e43ba76343f205f16da8e1e2be0821d
rstanuwijaya <rstanuwijaya@connect.ust.hk>
2021-09-10 22:36:09 +0800

    Update to textbox

--- bot.py
+++ bot.py
@@ -8,14 +8,17 @@ import pathlib
 import json
 import requests
 import pymongo
+import textwrap
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from pilmoji import Pilmoji
 
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
+import textwrap
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -35,26 +38,28 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(help='Warmest welcome message')
-# @commands.has_role('not geh')
 async def hello(ctx, *args):
     msg = 'Salam kenal, jancok kalian semua'
     await ctx.send(f'{msg} {", ".join(args)}')
 
 @bot.command()
 async def quote(ctx, *args):
+    print('ctx', ctx)
+    print('args', args)
     if len(args) < 2:
         await ctx.send("Usage: quotes [background] \"[quotes_text]\" ")
     else:
         name = args[0]
-        config = collections_images.find_one({"name": name})
+        config: dict = collections_images.find_one({"name": name})
         if config is None: 
             await ctx.send("Image template not found")
             return
         response = requests.get(config['image_url'])
         img = Image.open(io.BytesIO(response.content))
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("fonts/SansSerif.ttf", config['font_size'])
-        draw.text((config['x'], config['y']), args[1] ,(255,255,255),font=font)
+        font = ImageFont.truetype("fonts/" + config.get("font_family", "TimesNewRoman") + ".ttf", config['font_size'])
+        quote = "\n".join(textwrap.wrap(args[1], width=config.get("width", 20)))
+        with Pilmoji(img) as pilmoji:
+            pilmoji.text((config['x'], config['y']), quote, (255,255,255), font)
         with io.BytesIO() as image_binary:
             img.save(image_binary, 'PNG')
             image_binary.seek(0)
--- populate_db.py
+++ populate_db.py
@@ -4,9 +4,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-mongo_user = os.getenv('MONGODB_USER')
-mongo_pass = os.getenv('MONGODB_PASS')
-mongo_conn_str = f"mongodb+srv://{mongo_user}:{mongo_pass}@cluster0.ac4ho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongo_uri = os.getenv('MONGODB_URI')
+mongo_conn_str = f"mongodb+srv://{mongo_uri}/myFirstDatabase?retryWrites=true&w=majority"
 client = pymongo.MongoClient(mongo_conn_str)
 db_quotes = client['quotes']
 collections_images = db_quotes['image']
@@ -16,15 +15,19 @@ lao_ping_obj = {
     "name" : "lao_ping",
     "x" : 500,
     "y" : 100,
-    "font_size" : 40,
-    "image_url": "https://i.imgur.com/OvshW5S.png"
+    "width": 40,
+    "font_size" : 24,
+    "font_family": "TimesNewRoman",
+    "image_url": "https://i.imgur.com/OvshW5S.png",
 }
 
 lao_tzu_obj = {
     "name" : "lao_tzu",
     "x" : 500,
     "y" : 100,
-    "font_size" : 40,
+    "width": 40,
+    "font_size" : 24,
+    "font_family": "TimesNewRoman",
     "image_url": "https://i.imgur.com/N6uYFSO.png"
 }
 
