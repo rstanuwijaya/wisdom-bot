7e11b7cfde9df5ab09b77599eeefefbcfd351770
rstanuwijaya <rstanuwijaya@connect.ust.hk>
2021-09-21 12:33:14 +0800

    Add removed and move feature

--- components/music.py
+++ components/music.py
@@ -63,7 +63,7 @@ class VoiceState:
         self.player = None
         self.text_channel = None
         self.voice_client = None
-        self.queue = []
+        self.queue: list[VoiceEntry] = []
         self.play_next_song = asyncio.Event()
 
     async def play(self, query):
@@ -103,6 +103,28 @@ class VoiceState:
         self.queue.append(entry)
         return video_title, video_duration
 
+    def pop(self, index):
+        """pop at the specified index"""
+        if index == 0: return None #should not edit the first element
+        try:
+            entry = self.queue.pop(index)
+        except IndexError as exc:
+            raise exc
+        return entry
+
+    def insert(self, index, entry):
+        """insert after the specified index"""
+        self.queue.insert(index, entry)
+        return True
+
+    def get_display_queue(self):
+        """get formatted string of queue"""
+        formatted_string = ""
+        formatted_string += f"Now Playing: {self.queue[0].title}, Requested by {self.queue[0].requester}\n\n"
+        for i in range(1, len(self.queue)):
+            formatted_string += f"{i}. {self.queue[i].title}, Requested by {self.queue[i].requester}\n"
+        return formatted_string
+
     def after_finished(self, exc=None):
         try:
             fut = asyncio.run_coroutine_threadsafe(self.next(), self.bot.loop)
@@ -130,7 +152,7 @@ class Music(commands.Cog):
             await voice_state.start(ctx)
         else:
             title, duration = await voice_state.enqueue(ctx, query)
-        await ctx.send(f'Adding to queue: {title}\n Duration: {duration}')
+        await ctx.send(f'Adding to queue: {title}\nDuration: {duration//60}m{duration%60}s')
 
     @commands.command(aliases=['fs'])
     async def skip(self, ctx: commands.Context):
@@ -141,7 +163,7 @@ class Music(commands.Cog):
         await voice_state.skip()
         await ctx.send('Skipping!')
 
-    @commands.command()
+    @commands.command(aliases=['v'])
     async def volume(self, ctx, volume: int):
         """Changes the player's volume"""
 
@@ -161,11 +183,47 @@ class Music(commands.Cog):
     async def queue(self, ctx):
         """Show the current queue"""
         voice_state = self.get_voice_state(ctx.guild.id)
-        queue = voice_state.queue
-        formatted_string = ""
-        for i in range(len(queue)):
-            formatted_string += f"{i+1}. {queue[i].title} Requested by {queue[i].requester}\n"
+        formatted_string = voice_state.get_display_queue()
         await ctx.send(formatted_string)
+
+    @commands.command(aliases=['mv'])
+    async def move(self, ctx, *args):
+        """Move song queue"""
+        voice_state = self.get_voice_state(ctx.guild.id)
+        try:
+            if len(args) < 1:
+                args = (-1, 1)
+            elif len(args) < 2:
+                args = (int(args[0]), 1)
+            else:
+                args = (int(args[0]), int(args[1]))
+        except ValueError as exc:
+            await ctx.send(f'Wrong arguments')
+            raise exc
+        removed_elem = voice_state.pop(args[0])
+        if not removed_elem: 
+            await ctx.send(f'Index not found')
+            return
+        voice_state.insert(args[1], removed_elem)
+        await ctx.send(f'Moved removed_elem{removed_elem.title} from #{args[0]} to #{args[1]}')
+
+    @commands.command(aliases=['rm'])
+    async def remove(self, ctx, *args):
+        """Remove song queue"""
+        try:
+            if len(args) < 1:
+                args = (-1,)
+            else:
+                args = (int(args[0]),)
+        except ValueError as exc:
+            await ctx.send(f'Wrong arguments')
+            raise exc
+        voice_state = self.get_voice_state(ctx.guild.id)
+        removed_elem = voice_state.pop(args[0])
+        if not removed_elem: 
+            await ctx.send(f'Index not found')
+            return
+        await ctx.send(f'Removed {removed_elem.title}')
 
     @play.before_invoke
     async def ensure_voice(self, ctx):
